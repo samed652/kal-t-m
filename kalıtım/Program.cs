68f0b33575ac874478011622646a4b9eca5a9226
samed652 <samed652@BPC05>
2023-12-14 14:53:08 +0300

    Proje dosyası ekle.

---
+++ kalıtım/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalıtım
+{
+    public class OkulPersoneli
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+    public class Ogretmen : OkulPersoneli
+    {
+        public string Brans { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ogretmen ogr = new Ogretmen
+            {
+                Ad = "Ahmet",
+                Soyad = "Öz",
+                Brans = "Matematik"
+            };
+           
+
+            OkulPersoneli per = ogr; // !!!
+            Console.WriteLine(per.Ad);
+        }
+    }
+  
+            
+}
+    
